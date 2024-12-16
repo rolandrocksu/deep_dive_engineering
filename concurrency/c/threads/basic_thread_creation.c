d8a06449440bacb730dcf706f0b5f046a56e6b31
rolandrocking <rolandrocking@gmail.com>
2024-12-16 23:16:35 +0400

    Added basic thread creation in c

--- concurrency/c/threads/basic_thread_creation.c
+++ concurrency/c/threads/basic_thread_creation.c
@@ -5,21 +5,23 @@
 // TODO 
 void* print_message(void* arg) {
     int thread_id = *(int*)arg;
-    printf("Thread %d is running!", thread_id);
+    printf("Thread %d is running! \n", thread_id);
     return NULL;
 }
 
 int main() {
     pthread_t threads[3];
+    int threads_ids[3];
 
-    for (int i=1; i<4; i++){
-        if (pthread_create(&threads[i], NULL, print_message, &i) != 0) {
+    for (int i=0; i<3; i++){
+        threads_ids[i] = i+1;
+        if (pthread_create(&threads[i], NULL, print_message, &threads_ids[i]) != 0) {
             perror("Failed to create thread");
             return 1;
         }
     }
 
-    for (int i=1; i<4; i++){
+    for (int i=0; i<3; i++){
         if (pthread_join(threads[i], NULL) != 0) {
             perror("Failed to join thread");
             return 1;
